9fec1a59e4ba1e034040a9f967d34053af5ebaea
lucaslima313 <l.lucas15@escola.pr.gov.br>
2025-08-28 14:30:12 -0300

    Add files via upload

---
+++ flashcards-lucas/perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'barbie',
+    'como é onome do namorado da barbie?',
+    'ken'
+)
+
+    criaCartao(
+    'musica',
+    'qual a musica mais ouvida de 2025?',
+    'bonde das oncinhas'
+)
+
+criaCartao(
+    "desenhos",
+    "qual desenho mais assistido dos ultimos anos?",
+    "dragon ball"
+)
+
+criaCartao(
+    "geografia",
+    "qual a capital da fança?",
+    "é paris"
+)
+criaCartao(
+    'dança',
+    'Quais são os principais estilos de dança?',
+    'balé, dança contemporânea, jazz, sapateado, danças folclóricas, etc'
+)
+
+criaCartao(
+    'dança',
+    'Quais são os elementos básicos de uma dança?',
+    'espaço, tempo, força, fluidez, direção, níveis,'
+)
+
+criaCartao(
+    'dança',
+    'Como a dança pode ser usada para expressar emoções e contar histórias?',
+    'a dança pode expressar alegria, tristeza, raiva, amor, etc., e pode ser usada para contar histórias e transmitir mensagens'
+)
+criaCartao(
+    'dança',
+    ' Quais são os benefícios da prática da dança para a saúde física e mental?',
+    'melhora a coordenação motora, aumenta a flexibilidade, reduz o estresse, melhora a autoestima'
+)
+criaCartao(
+    'dança',
+    'Qual a importância da música na dança?',
+    'a música pode influenciar o ritmo, o movimento e a expressão da dança, e pode ser usada para criar a atmosfera e o clima de uma apresentação.'
+)
